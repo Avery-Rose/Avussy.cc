6eee3ede9900b184c6a0c26db056e35b27a4c4c0
Avery <avery.doucet03@gmail.com>
2022-05-20 18:02:41 -0300

    push

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+
+
+
+const { Task, List, User } = initSchema(schema);
+
+export {
+  Task,
+  List,
+  User
+};
